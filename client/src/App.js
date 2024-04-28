81fb321e88a6613df7ff82bc209c387257afb3f2
cliff <cliffmudigo5@gmail.com>
2024-04-28 12:37:36 +0300

    :art: setting theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-
+import { Button } from "antd";
 function App() {
   return (
     <div className="App">
         <h1>balez MP</h1>
+        <Button type="primary">Antd Button</Button>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ConfigProvider } from 'antd';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider
+          theme ={{
+            components :{
+              Button : {
+                colorPrimary : '#40513B',
+                colorPrimaryHover : '#40513B',
+                borderRadius : 0
+
+              }
+            }
+          }}
+    
+    >
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
 
