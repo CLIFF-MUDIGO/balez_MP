9098e9f5fb9aada788c0edaca54d137064ed335a
cliff <cliffmudigo5@gmail.com>
2024-05-01 11:19:03 +0300

    :construction: Registration and Login Backend

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 require('dotenv').config();
 const dbConfig = require('./config/dbConfig');
 const port = process.env.PORT || 5000;
+
+
+
+const userRoutes = require('./routes/usersRoutes');
+app.use('/api/users', userRoutes);
 
 app.listen(port, () => console.log(`Node/Express server started on port ${port}`));
