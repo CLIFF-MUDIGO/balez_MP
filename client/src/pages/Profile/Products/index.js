a1e359754b5ca23d2ebe80f2e9b563b1d368e2dc
cliff <cliffmudigo5@gmail.com>
2024-05-28 11:23:00 +0300

    product table

--- client/src/pages/Profile/Products/index.js
+++ client/src/pages/Profile/Products/index.js
@@ -1,23 +1,97 @@
-import { Button } from 'antd'
-import React from 'react'
-
-import ProductsForm from './ProductsForm'
+import { Button, Table, message } from 'antd';
+import React, { useEffect } from 'react';
+import ProductsForm from './ProductsForm';
+import { useDispatch } from 'react-redux';
+import { GetProducts } from '../../../apicalls/products';
+import { setLoader } from '../../../redux/loadersSlice';
 
 function Products() {
-  const [showProductForm, setShowProductForm] = React.useState(false)
+  const [products, setProducts] = React.useState([]);
+  const [showProductForm, setShowProductForm] = React.useState(false);
+  const dispatch = useDispatch();
+
+  const getData = async () => {
+    try {
+      dispatch(setLoader(true));
+      const response = await GetProducts();
+      dispatch(setLoader(false));
+      if (response.success) {
+        // Add unique keys to each product
+        const productsWithKeys = response.products.map((product) => ({ ...product, key: product._id }));
+        setProducts(productsWithKeys);
+      } else {
+        message.error(response.message);
+      }
+    } catch (error) {
+      dispatch(setLoader(false));
+      message.error(error.message);
+    }
+  };
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Description',
+      dataIndex: 'description',
+    },
+    {
+      title: 'Price',
+      dataIndex: 'price',
+    },
+    {
+      title: 'Category',
+      dataIndex: 'category',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+    },
+    {
+      title: 'Action',
+      dataIndex: 'action',
+      render: (text, record) => {
+
+        return<div className="flex gap-5">
+            <i className="ri-delete-bin-line"></i>
+            <i className="ri-edit-2-line"></i>
+          </div>
+      }
+        
+
+    },
+  ];
+
+  const handleEdit = (record) => {
+    // Handle edit action
+    setShowProductForm(true);
+    // Additional logic to populate the form with product details can go here
+  };
+
+  useEffect(() => {
+    getData();
+  }, []); // Correct placement of the dependency array
+
   return (
     <div>
-      <div className='flex justify-end'>
+      <div className='flex justify-end mb-2 '>
         <Button
           type="default"
           onClick={() => setShowProductForm(true)}
         >
           Add Product
         </Button>
-      </div>
-         {showProductForm && <ProductsForm showProductForm={showProductForm} setShowProductForm={setShowProductForm} />}
+      </div> 
+      <Table columns={columns} dataSource={products} />
+      {showProductForm && <ProductsForm showProductForm={showProductForm} setShowProductForm={setShowProductForm} />}
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
--- server/routes/productsRoute.js
+++ server/routes/productsRoute.js
@@ -2,10 +2,7 @@ const router = require('express').Router();
 const Product = require('../models/productModel');
 const authMiddleware = require('../middlewares/authMiddleware');
 
-
-
-
-// add a new product
+// Add a new product
 router.post('/add-product', authMiddleware, async (req, res) => {
     try {
         const newProduct = new Product(req.body);
@@ -13,29 +10,29 @@ router.post('/add-product', authMiddleware, async (req, res) => {
         res.send({
             success: true,
             message: 'Product added successfully'
-        })
+        });
     } catch (error) {
         res.send({
             success: false,
             message: error.message
-        })
+        });
     }
 });
 
-// get all products
+// Get all products
 router.get('/get-products', async (req, res) => {
     try {
         const products = await Product.find();
         res.send({
             success: true,
-            data: products
-        })
+            products // <-- Ensure this matches the key expected in your frontend
+        });
     } catch (error) {
         res.send({
             success: false,
             message: error.message
-        })
+        });
     }
 });
 
-module.exports = router
+module.exports = router;
